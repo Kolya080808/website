e82a331ae3bd0818658cce13c8242a37d07c2ec4
Nikolay <kravtsovnd08@gmail.com>
2024-06-21 13:27:26 +0300

    Added one telescope, "hello" from the channel and some hrefs

--- shop/internetshop/views.py
+++ shop/internetshop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def homepage(request):
-    return HttpResponse("<a href='https://www.youtube.com/watch?v=j-iheFkstFQ'>Отличная штука, этот текст. Нажми на меня.</a>")
+def home(request):
+    return render(request, "index.html")
