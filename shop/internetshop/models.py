4265d4892fd787026f29598693f1608351481252
Nikolay <kravtsovnd08@gmail.com>
2024-06-28 17:43:32 +0300

    сделал отступ для лого чуть подправил текст, исправил баги с рамками, поменял шрифт, добавил товар, подвинул кнопки, подправил дизайн, поставил товары в центр, сделал кнопки нормального дизайна, добавил папку для стилей и вынес все в нее, добавил модель Product, добавил два продукта, админку для продуктов.

--- shop/internetshop/models.py
+++ shop/internetshop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    official_or_not_description=models.IntegerField(max_length=2)
+    name=models.CharField(max_length=128)
+    description=models.CharField(max_length=2048)
+    price = models.IntegerField()
+    image_url=models.CharField(max_length=256)
