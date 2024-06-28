1f20984800e5344b78a87bd62ed56e8435b1a783
Nikolay <kravtsovnd08@gmail.com>
2024-06-28 18:48:39 +0300

    попытался сделать чтобы обьекты отображались на сайте, но чет не получается

--- shop/internetshop/models.py
+++ shop/internetshop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    official_or_not_description=models.IntegerField(max_length=2)
+    official_or_not_description=models.CharField(max_length=37)
     name=models.CharField(max_length=128)
     description=models.CharField(max_length=2048)
     price = models.IntegerField()
